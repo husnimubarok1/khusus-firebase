bba81254ee91daf965b229784b5b016137bcabd4
husnimubarok1 <husni.mubarok.16@mail.ugm.ac.id>
2023-09-08 21:53:20 +0700

    app firebase

---
+++ login/script.js
@@ -0,0 +1,9 @@
+function passToText() {
+    let pass = document.getElementById('password');
+    pass.type = 'text';
+}
+
+function oriText () {
+    let text = document.getElementById('password');
+    text.type = 'password';
+}
